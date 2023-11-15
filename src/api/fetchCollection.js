59e4e24ba3b15089f3bfb1049eff37b6d8f790ef
Mvlprem <mvl.prem@outlook.com>
2023-11-15 13:02:25 +0530

    refactor: fetch data

--- src/api/fetchCollection.js
+++ src/api/fetchCollection.js
@@ -3,17 +3,38 @@ const collectionId = [
   429493977110, 429512622102, 429531037718,
 ]
 
+function urlBuilder(id) {
+  const BASE_URL = 'https://mock.shop/api?query='
+  const COLLECTION = `{collection(id:"gid://shopify/Collection/${id}"){id,handle,title,description,image{url},`
+  const PRODUCTS =
+    'products(first: 20){edges{node{id,title,description,featuredImage{url},variants(first:1){edges{node{price{amount}}}}}}}}}'
+
+  return BASE_URL + COLLECTION + PRODUCTS
+}
+
+function refactorArray(oldArray) {
+  const newArray = oldArray.map((product) => {
+    const price = product.node.variants.edges[0].node.price.amount
+
+    product.node.image = product.node.featuredImage
+    product.node.price = price
+
+    delete product.node.variants
+    delete product.node.featuredImage
+
+    return product
+  })
+  return newArray
+}
+
 const fetchCollection = async (controller) => {
   const collections = []
   for (const id of collectionId) {
-    const request = await fetch(
-      `https://mock.shop/api?query={collection(id:"gid://shopify/Collection/${id}"){id,handle,title,description,image{id,url},products(first: 20){edges{node{id,title,featuredImage{id,url}}}}}}`,
-      { signal: controller.signal }
-    )
+    const request = await fetch(urlBuilder(id), { signal: controller.signal })
     const response = await request.json()
     const collection = response.data.collection
     const products = response.data.collection.products.edges
-    collection.products = products
+    collection.products = refactorArray(products)
     collections.push(collection)
   }
   return collections
