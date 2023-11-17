e4587dc58748434ccef35a6b02abeab3f8a7b5b8
Mvlprem <mvl.prem@outlook.com>
2023-11-17 20:21:35 +0530

    feat: router config

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,11 +1,44 @@
 import App from '../App.jsx'
+import Cart from '../pages/Cart.jsx'
+import Home from '../pages/Home.jsx'
+import Error from '../pages/Error.jsx'
+import Product from '../pages/Product.jsx'
+import ProductList from '../pages/ProductList.jsx'
+import Collections from '../pages/Collections.jsx'
+import ImageViewer from '../pages/ImageViewer.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-    children: [],
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: 'cart',
+        element: <Cart />,
+      },
+      {
+        path: 'products',
+        element: <ProductList />,
+      },
+      {
+        path: 'product',
+        element: <Product />,
+      },
+      {
+        path: 'collections',
+        element: <Collections />,
+      },
+    ],
+  },
+  {
+    path: 'preview',
+    element: <ImageViewer />,
   },
 ])
 
