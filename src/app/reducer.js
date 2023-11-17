95648fbae466718ab721dcc83be694db3e653ff7
Mvlprem <mvl.prem@outlook.com>
2023-11-17 20:19:30 +0530

    feat: remove and delete from cart

--- src/app/reducer.js
+++ src/app/reducer.js
@@ -1,5 +1,7 @@
 export const ACTIONS = {
   ADD: 'add',
+  REMOVE: 'remove',
+  DELETE: 'delete',
 }
 
 function reducer(state, action) {
@@ -7,26 +9,50 @@ function reducer(state, action) {
     case ACTIONS.ADD: {
       return addToCart(state, action.product, action.quantity)
     }
+    case ACTIONS.REMOVE: {
+      return removeFromCart(state, action.product, action.quantity)
+    }
+    case ACTIONS.DELETE: {
+      return deleteFromCart(state, action.product)
+    }
     default: {
       return state
     }
   }
 }
 
-/* Add to cart Logic */
+/* add to cart Logic */
 const addToCart = (cart, product, quantity) => {
-  const duplicateProduct = cart.some((element) => element.node.id == product.id)
+  const duplicateProduct = cart.some(
+    (element) => element.node.id === product.id
+  )
   const newProduct = { node: product, quantity: quantity }
 
   if (!duplicateProduct) {
     return [...cart, newProduct]
   } else if (duplicateProduct) {
     return cart.map((element) => {
-      if (element.node.id == product.id) {
+      if (element.node.id === product.id) {
         return { node: element.node, quantity: element.quantity + quantity }
       } else return element
     })
   } else return cart
+}
+
+/* remove from cart Logic */
+const removeFromCart = (cart, product, quantity) => {
+  if (quantity < 2) return deleteFromCart(cart, product)
+  else
+    return cart.map((element) => {
+      if (element.node.id === product.id) {
+        return { node: element.node, quantity: element.quantity - 1 }
+      } else return element
+    })
+}
+
+/* delete from cart Logic */
+const deleteFromCart = (cart, product) => {
+  return cart.filter((element) => element.node.id !== product.id)
 }
 
 export default reducer
