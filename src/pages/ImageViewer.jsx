09aedf7580d8b3f8d2230be0b596b1291ed24126
Mvlprem <mvl.prem@outlook.com>
2023-11-20 10:39:19 +0530

    feat: zoom pan pinch image

--- src/pages/ImageViewer.jsx
+++ src/pages/ImageViewer.jsx
@@ -1,15 +1,22 @@
 import { useLocation } from 'react-router-dom'
+import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch'
 
 function ImageViewer() {
   const { state } = useLocation()
 
   return (
     <>
-      <div></div>
       <main className="d-flex align-items-center">
-        <img src={state.url} alt="preview" className="img-fluid" />
+        <TransformWrapper>
+          <TransformComponent>
+            <div className="row d-flex justify-content-center">
+              <div className="col-12 col-md-8 col-lg-6">
+                <img src={state.url} alt="preview" className="img-fluid" />
+              </div>
+            </div>
+          </TransformComponent>
+        </TransformWrapper>
       </main>
-      <div></div>
     </>
   )
 }
